6b264d5fc0b0983fc740325cc53e39f1f9e3cad5
RowanMeara <rowan@bebo.com>
2018-02-12 20:52:27 -0800

    Modify subprocess wrapper.

--- src/subprocess.cpp
+++ src/subprocess.cpp
@@ -1,16 +1,13 @@
+#include <iostream>
 #include <Windows.h>
 
 #include <include/cef_app.h>
+#include "cef_app.h"
 
-// Program entry-point function.
-int main(int argc, char *argv[]) {
-  // Structure for passing command-line arguments.
-  // The definition of this structure is platform-specific.
-  CefMainArgs main_args(GetModuleHandle(NULL));
-
-  // Optional implementation of the CefApp interface.
-  /* CefRefPtr<Browser> app(new Browser); */
-
-  // Execute the sub-process logic. This will block until the sub-process should exit.
-  return CefExecuteProcess(main_args, nullptr, nullptr);
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	CefMainArgs mainArgs(NULL);
+	// CefRefPtr<Browser> app(new Browser);
+	return CefExecuteProcess(mainArgs, NULL, NULL);
 }
