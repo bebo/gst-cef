8d2bc387217306121d3e0a167ece44b4a4a2f51d
Rowan Meara <rowanmeara@gmail.com>
2018-02-12 21:24:01 -0800

    Fix main function

--- src/subprocess.cpp
+++ src/subprocess.cpp
@@ -4,10 +4,10 @@
 #include <include/cef_app.h>
 #include "cef_app.h"
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
 {
-	CefMainArgs mainArgs(NULL);
+	CefMainArgs mainArgs(hInstance);
 	// CefRefPtr<Browser> app(new Browser);
 	return CefExecuteProcess(mainArgs, NULL, NULL);
 }
