425ab9a0897df86cff23a6950bc9c80ceb7a6aa2
RowanMeara <rowan@bebo.com>
2018-02-16 16:43:27 -0800

    Jenkins should upload to S3

--- jenkins_build.cmd
+++ jenkins_build.cmd
@@ -2,3 +2,9 @@ mkdir build
 cd build
 cmake -G "Visual Studio 15 2017 Win64" .. -DUSE_SANDBOX=0
 cmake --build . --config Release
+
+set FILENAME=%JOBNAME%_%TAG%.zip
+"C:\Program Files\7-Zip\7z.exe" a -r ..\%FILENAME% -w .\dist\* -mem=AES256
+
+cd ..
+aws s3api put-object --bucket bebo-app --key repo/gst-cef/%FILENAME% --body %FILENAME%
