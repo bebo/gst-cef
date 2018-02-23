a9f5b730308545cce8977509abb057050dec6c28
Rowan Meara <rowanmeara@gmail.com>
2018-02-22 20:08:20 -0800

    Alerts appear to work the first time.

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -1,62 +1,68 @@
 import sys
 import gi
+import time
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject
 Gst.init(None)
 
 import threading
 
-
 def run_main_loop():
     loop = GObject.MainLoop()
     loop.run()
 
-runner = threading.Thread(target=run_main_loop)
-runner.start()
 
-# Create the elements
-source = Gst.ElementFactory.make("cef")
-sink = Gst.ElementFactory.make("autovideosink")
+def main():
+    runner = threading.Thread(target=run_main_loop)
+    runner.start()
 
-# Create the empty pipeline
-pipeline = Gst.Pipeline.new("test-pipeline")
+    # Create the elements
+    source = Gst.ElementFactory.make("cef")
+    sink = Gst.ElementFactory.make("autovideosink")
 
-if not source or not sink or not pipeline:
-    print("Not all elements could be created.")
-    exit(-1)
+    # Create the empty pipeline
+    pipeline = Gst.Pipeline.new("test-pipeline")
 
-# Build the pipeline
-pipeline.add(source)
-pipeline.add(sink)
-if not Gst.Element.link(source, sink):
-    print("Elements could not be linked.")
-    exit(-1)
+    if not source or not sink or not pipeline:
+        print("Not all elements could be created.")
+        exit(-1)
 
-# Modify the source's properties
-# source.set_property("pattern", 0)
+    # Build the pipeline
+    pipeline.add(source)
+    pipeline.add(sink)
+    if not Gst.Element.link(source, sink):
+        print("Elements could not be linked.")
+        exit(-1)
 
-# Start playing
-ret = pipeline.set_state(Gst.State.PLAYING)
-if ret == Gst.StateChangeReturn.FAILURE:
-    print("Unable to set the pipeline to the playing state.")
-    exit(-1)
+    # Start playing
+    ret = pipeline.set_state(Gst.State.PLAYING)
+    if ret == Gst.StateChangeReturn.FAILURE:
+        print("Unable to set the pipeline to the playing state.")
+        exit(-1)
 
-# Wait until error or EOS
-bus = pipeline.get_bus()
-msg = bus.timed_pop_filtered(
-    Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+    # Modify the source's properties
+    time.sleep(2)
+    source.set_property('javascript', 'alert("test");')
 
-# Parse message
-if (msg):
-    if msg.type == Gst.MessageType.ERROR:
-        err, debug = msg.parse_error()
-        print("Error received from element %s: %s" % (
-            msg.src.get_name(), err))
-        print("Debugging information: %s" % debug)
-    elif msg.type == Gst.MessageType.EOS:
-        print("End-Of-Stream reached.")
-    else:
-        print("Unexpected message received.")
+    # Wait until error or EOS
+    bus = pipeline.get_bus()
+    msg = bus.timed_pop_filtered(
+        Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
 
-# Free resources
-pipeline.set_state(Gst.State.NULL)
+    # Parse message
+    if (msg):
+        if msg.type == Gst.MessageType.ERROR:
+            err, debug = msg.parse_error()
+            print("Error received from element %s: %s" % (
+                msg.src.get_name(), err))
+            print("Debugging information: %s" % debug)
+        elif msg.type == Gst.MessageType.EOS:
+            print("End-Of-Stream reached.")
+        else:
+            print("Unexpected message received.")
+
+    # Free resources
+    pipeline.set_state(Gst.State.NULL)
+
+if __name__ == '__main__':
+    main()
