fe029d9f569d69eb39a4a455bc53a29509493ed1
Rowan Meara <rowanmeara@gmail.com>
2018-02-22 18:27:09 -0800

    Add start of test script.

---
+++ test/test_pipeline.py
@@ -0,0 +1,52 @@
+import sys
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+Gst.init(None)
+ 
+# Create the elements
+source = Gst.ElementFactory.make("videotestsrc")
+sink = Gst.ElementFactory.make("autovideosink")
+
+# Create the empty pipeline
+pipeline = Gst.Pipeline.new("test-pipeline")
+
+if not source or not sink or not pipeline:
+    print("Not all elements could be created.")
+    exit(-1)
+
+# Build the pipeline
+pipeline.add(source)
+pipeline.add(sink)
+if not Gst.Element.link(source, sink):
+    print("Elements could not be linked.")
+    exit(-1)
+
+# Modify the source's properties
+# source.set_property("pattern", 0)
+
+# Start playing
+ret = pipeline.set_state(Gst.State.PLAYING)
+if ret == Gst.StateChangeReturn.FAILURE:
+    print("Unable to set the pipeline to the playing state.")
+    exit(-1)
+
+# Wait until error or EOS
+bus = pipeline.get_bus()
+msg = bus.timed_pop_filtered(
+    Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+
+# Parse message
+if (msg):
+    if msg.type == Gst.MessageType.ERROR:
+        err, debug = msg.parse_error()
+        print("Error received from element %s: %s" % (
+            msg.src.get_name(), err))
+        print("Debugging information: %s" % debug)
+    elif msg.type == Gst.MessageType.EOS:
+        print("End-Of-Stream reached.")
+    else:
+        print("Unexpected message received.")
+
+# Free resources
+pipeline.set_state(Gst.State.NULL)
