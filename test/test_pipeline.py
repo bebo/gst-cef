6226eb153b8ade46b17598bed8ca93159105a4c0
RowanMeara <rrm89@cornell.edu>
2018-03-12 21:20:00 -0700

    Update test script

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -7,12 +7,15 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject
 Gst.init(None)
 
+loop = None
 
 
 def run_main_loop(loop):
     loop.run()
 
+
 def main():
+    global loop
     print('The python.exe that this script is called with must be in the same directory as the cef resource files')
     loop = GObject.MainLoop()
     runner = threading.Thread(target=run_main_loop, args=(loop,))
@@ -20,11 +23,9 @@ def main():
 
     # Create the elements
     queue1 = Gst.ElementFactory.make('queue')
-    queue2 = Gst.ElementFactory.make('queue')
     source = Gst.ElementFactory.make('cef')
     #source = Gst.ElementFactory.make('videotestsrc')
-    sink = Gst.ElementFactory.make('autovideosink')
-    sink2 = Gst.ElementFactory.make('glimagesink')
+    sink = Gst.ElementFactory.make('glimagesink')
     tee = Gst.ElementFactory.make('tee')
 
     # Create the empty pipeline
@@ -38,15 +39,11 @@ def main():
     pipeline.add(source)
     pipeline.add(sink)
     pipeline.add(tee)
-    pipeline.add(sink2)
     pipeline.add(queue1)
-    pipeline.add(queue2)
 
     Gst.Element.link(source, tee)
     Gst.Element.link(tee, queue1)
-    Gst.Element.link(tee, queue2)
-    Gst.Element.link(queue1, sink2)
-    Gst.Element.link(queue2, sink)
+    Gst.Element.link(queue1, sink)
 
     source.set_property('initialization-js', "document.getElementById('gsr').innerText ='0'")
     # Start playing
@@ -65,5 +62,60 @@ def main():
     loop.quit()
 
 
+def latency_query():
+    global loop
+    print('The python.exe that this script is called with must be in the same directory as the cef resource files')
+    loop = GObject.MainLoop()
+    runner = threading.Thread(target=run_main_loop, args=(loop,))
+    runner.start()
+
+    # Create the elements
+    queue1 = Gst.ElementFactory.make('queue')
+    #source = Gst.ElementFactory.make('cef')
+    source = Gst.ElementFactory.make('videotestsrc')
+    sink = Gst.ElementFactory.make('glimagesink')
+    tee = Gst.ElementFactory.make('tee')
+    mixer = Gst.ElementFactory.make('glvideomixer')
+
+    # Create the empty pipeline
+    pipeline = Gst.Pipeline.new("test-pipeline")
+
+    if not source or not sink or not pipeline:
+        print("Not all elements could be created.")
+        exit(-1)
+
+    # Build the pipeline
+    pipeline.add(source)
+    pipeline.add(sink)
+    pipeline.add(tee)
+    pipeline.add(queue1)
+    pipeline.add(mixer)
+
+    Gst.Element.link(source, tee)
+    Gst.Element.link(tee, queue1)
+    Gst.Element.link(queue1, mixer)
+    Gst.Element.link(mixer, sink)
+
+    # Start playing
+    ret = pipeline.set_state(Gst.State.PLAYING)
+    if ret == Gst.StateChangeReturn.FAILURE:
+        print("Unable to set the pipeline to the playing state.")
+        exit(-1)
+
+    time.sleep(2)
+    for i in range(1, 5):
+        time.sleep(2)
+        print('Latency: {}'.format(pipeline.get_latency()))
+    time.sleep(20)
+    pipeline.set_state(Gst.State.NULL)
+    print('Pipeline state set to null.  Sleeping for 10s')
+    time.sleep(3)
+    loop.quit()
+
+
 if __name__ == '__main__':
-    main()
+    try:
+        # latency_query()
+        main()
+    except KeyboardInterrupt:
+        loop.quit()
