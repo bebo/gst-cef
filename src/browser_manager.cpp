f1baaf0ca9ac2afc06c9bcb9794ed902c359c951
Rowan Meara <rowanmeara@gmail.com>
2018-03-20 23:23:13 -0700

    allow file access.

--- src/browser_manager.cpp
+++ src/browser_manager.cpp
@@ -160,6 +160,7 @@ void Browser::OnBeforeCommandLineProcessing(
     const CefString &process_type,
     CefRefPtr<CefCommandLine> command_line)
 {
+  command_line->AppendSwitch("allow-file-access-from-files");
   command_line->AppendSwitch("disable-gpu");
   command_line->AppendSwitch("disable-gpu-compositing");
   command_line->AppendSwitch("enable-begin-frame-scheduling");
--- src/subprocess.cpp
+++ src/subprocess.cpp
@@ -21,6 +21,7 @@ void BrowserApp::OnBeforeCommandLineProcessing(
     const CefString &process_type,
     CefRefPtr<CefCommandLine> command_line)
 {
+  command_line->AppendSwitch("allow-file-access-from-files");
   command_line->AppendSwitch("disable-gpu");
   command_line->AppendSwitch("disable-gpu-compositing");
   command_line->AppendSwitch("enable-begin-frame-scheduling");
