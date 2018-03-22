19396ca40fc6f93575d1998196215a20fa819de1
RowanMeara <rrm89@cornell.edu>
2018-03-22 15:14:37 -0700

    Add streamlabs alert test pipeline.

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -115,9 +115,47 @@ def latency_query():
     loop.quit()
 
 
+def streamlabs_alert():
+    global loop
+    print('The python.exe that this script is called with must be in the same directory as the cef resource files')
+    loop = GObject.MainLoop()
+    runner = threading.Thread(target=run_main_loop, args=(loop,))
+    runner.start()
+
+    # Create the elements
+    source = Gst.ElementFactory.make('cef')
+    sink = Gst.ElementFactory.make('glimagesink')
+    source.set_property('url', 'https://streamlabs.com/alert-box/v3/B32C8C284B4D610B2D79')
+    # Create the empty pipeline
+    pipeline = Gst.Pipeline.new("test-pipeline")
+
+    if not source or not sink or not pipeline:
+        print("Not all elements could be created.")
+        exit(-1)
+
+    # Build the pipeline
+    pipeline.add(source)
+    pipeline.add(sink)
+
+    Gst.Element.link(source, sink)
+
+    # Start playing
+    ret = pipeline.set_state(Gst.State.PLAYING)
+    if ret == Gst.StateChangeReturn.FAILURE:
+        print("Unable to set the pipeline to the playing state.")
+        exit(-1)
+
+    time.sleep(20)
+    pipeline.set_state(Gst.State.NULL)
+    print('Pipeline state set to null.  Sleeping for 10s')
+    time.sleep(3)
+    loop.quit()
+
+
 if __name__ == '__main__':
     try:
-        latency_query()
+        # latency_query()
         # main()
+        streamlabs_alert()
     except KeyboardInterrupt:
         loop.quit()
